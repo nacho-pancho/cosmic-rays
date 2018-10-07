412c1d9a930e125ce5a1f92f83e27f5ab7b2b66c
Ignacio Ramirez <nacho@fing.edu.uy>
2018-10-07 20:42:11 -0300

    Agregue un script para separar cada imagen en 2, ya que son distintos sensores en cada mitad

---
+++ code/1.separar.py
@@ -0,0 +1,31 @@
+import fitsio
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import io
+import os
+
+DATADIR = '../data/'
+RESDIR = '../results/'
+EXT = '.fits'
+
+os.system('mkdir -p ../' + RESDIR + 'cielo')
+os.system('mkdir -p ../' + RESDIR + 'dark')
+k = 0
+with open(DATADIR+'cielo.txt') as filelist:
+        for fname  in filelist:
+                fbase = fname[:-len(EXT)]
+                I = fitsio.read(DATADIR+fname)
+                #
+                # separamos en dos imagenes
+                #
+                hdr = fitsio.read_header(DATADIR+fname)
+                m,n = I.shape
+                I1 = I[:2048, 24:(24+2048)]
+                I2 = I[:2048, n-(2048+24):(n-24)]
+                fitsio.write(DATADIR+fbase+'1.fits',I1, extname='fits',compress='RICE')
+                fitsio.write(DATADIR+fbase+'2.fits',I2, extname='fits',compress='RICE')
+                k = k + 1
+                #
+                # falta: obtener lista de coordenadas normalizadas para pasarle al algoritmo de IPOL
+                #
+	#plt.show()
--- code/det_cr_en_darks.py
+++ code/det_cr_en_darks.py
@@ -2,30 +2,58 @@ import fitsio
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import io
-with open('darks.txt') as filelist:
-        for fname  in filelist:
-                img = fitsio.read(fname).astype(np.double)
-                hdr = fitsio.read_header(fname)
-                m,n = img.shape
-                mask = np.empty((m,n))
-                print hdr
-                for j in range(n):
-                        v = np.sort(img[:,j])
-                        t0 = v[m/2]
-                        t1 = v[95*m/100]
-                        u = t0 + (t1-t0)*5
-                        vi = np.var(v[:(90*m/100)])
-                        u = t0 + np.sqrt(vi)*10
-                        print 'j=',j,' t0=',t0,' t1=',t1,' u=',u,'var=',vi,' max=',v[-1]
-                        mask[:,j] = (img[:,j] > u).astype(np.double)
-                out = np.zeros((m,n,3))
-                img = np.log2(img+1)
-                img = img - np.min(img.ravel())
-                img = img*(1.0/np.max(img))
-                out[:,:,0] = mask
-                out[:,:,1] = img
-                io.imsave(fname[:-4]+"_pseudo.png",out)
-                io.imsave(fname[:-4]+"_det.png",mask)
-                #
-                # falta: obtener lista de coordenadas normalizadas para pasarle al algoritmo de IPOL
-                #
+import os
+
+DATADIR = '../data/'
+RESDIR = '../results/'
+EXT='.fits'
+os.system('mkdir -p ../' + RESDIR + 'cielo')
+os.system('mkdir -p ../' + RESDIR + 'dark')
+k = 0
+with open(DATADIR+'cielo_sep.txt') as filelist:
+    for fname  in filelist:
+        img = fitsio.read(DATADIR+fname).astype(np.double)
+        fbase = fname[:-len(EXT)]
+        fbase2 = fbase[fbase.rfind('/'):]
+        #
+        # separamos en dos imagenes
+        #
+        hdr = fitsio.read_header(DATADIR+fname)
+        m,n = img.shape
+        mask = np.empty((m,n))
+        print hdr
+        colstats = np.zeros((7,n))
+        for j in range(n):
+            v = np.sort(img[:,j])
+            p00 = v[0]
+            p10 = v[10*m/100]
+            p50 = v[m/2]
+            p90 = v[95*m/100]
+            p100 = v[-1]
+            rmean = np.mean(v[(10*m/100):(90*m/100)]) # media robusta
+            rvar =np.var(v[:(90*m/100)]) # varianza robusta
+            u1 = p50 + (p90-p50)*5 # otro umbral
+            u2 = rmean + np.sqrt(rvar)*5 # umbral de deteccion
+            #print 'j=',j,'p00=',p00,'p10',p10,' p50=',p50,' p90=',p90,' p100=',p100,' u1=',u1, 'u2=',u2
+            colstats[:,j] = np.array([p00,p10,p50,p90,p100,rmean,rvar])
+            mask[:,j] = (img[:,j] > u2).astype(np.double)
+        # desplegue
+        plt.figure(2*k+1)
+        plt.title(fname)
+        for i in range(6):
+            plt.subplot(2,3,i+1)
+            plt.plot(colstats[i,:])
+            plt.grid(True)
+        plt.savefig(RESDIR+fbase+'stats.png')
+        out = np.zeros((m,n,3))
+        img = np.log2(img+1)
+        img = img - np.min(img.ravel())
+        img = img*(1.0/np.max(img))
+        out[:,:,0] = mask
+        out[:,:,1] = img
+        plt.title(fname)
+        plt.figure(2*k+2)
+        plt.imshow(out)
+        io.imsave(RESDIR+fbase+"pseudo.png",out)
+        io.imsave(RESDIR+fbase+"det.png",mask)
+        k = k + 1
